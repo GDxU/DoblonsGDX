ffab722718e76fbc0ac5a3fbeea68f60a5e9f1e4
Micnubinub <lindelwencube.ln@gmail.com>
2016-11-26 03:01:58 +1000

    running, fix hidden bugs

--- core/src/tbs/doblon/io/views/EditText.java
+++ core/src/tbs/doblon/io/views/EditText.java
@@ -1,6 +1,7 @@
 package tbs.doblon.io.views;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 
 import tbs.doblon.io.Game;
 import tbs.doblon.io.GameController;
@@ -11,24 +12,26 @@ import tbs.doblon.io.Utility;
  */
 
 public class EditText extends View {
-    String text;
+    public String text;
     final GameController.KeyListener listener = new GameController.KeyListener() {
         @Override
         public void onKeyPresses(char c) {
-            final String s = String.valueOf(c);
-            if (s.length() != 1)
+            if (Character.getNumericValue(c) < 0)
                 return;
+
+            final String s = String.valueOf(c);
             if (text == null)
                 text = "";
             text = text.concat(s);
-            Utility.print(text);
         }
 
         @Override
         public void onBackSpace() {
+
             if (text != null && text.length() > 0) {
-                text = text.substring(0, text.length() - 1).trim();
+                text = text.substring(0, text.length() - 1);
             }
+
         }
     };
     float xOffset;
@@ -39,7 +42,6 @@ public class EditText extends View {
             public void onClick(View view, int x, int y) {
                 Gdx.input.setOnscreenKeyboardVisible(true);
                 GameController.setKeyListener(listener);
-                Utility.print("click > " + x + ", " + y);
             }
         });
     }
@@ -51,6 +53,6 @@ public class EditText extends View {
 
     @Override
     public void draw(float relX, float relY, float parentRight, float parentTop) {
-        Utility.drawCenteredText(Game.spriteBatch(), 0, text, Game.screenWidth / 2, Game.screenHeight / 2, Utility.getScale(10));
+        Utility.drawCenteredText(Game.spriteBatch(), text, Color.BLACK, Game.screenWidth / 2, Game.screenHeight / 2, Utility.getScale(1));
     }
 }
